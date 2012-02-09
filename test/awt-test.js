87c2083190d07bcdec39eaecdc1f081d03f2ed67
Joe Ferner <joe@fernsroth.com>
2012-02-08 22:53:11 -0500

    added verification that the awt option is set

--- test/awt-test.js
+++ test/awt-test.js
@@ -7,6 +7,9 @@ var path = require('path');
 
 exports['AWT'] = nodeunit.testCase({
   "calling AWT method (see issue 1)": function(test) {
+    var headlessVal = java.callStaticMethodSync("java.lang.System", "getProperty", "java.awt.headless");
+    console.log("java.awt.headless =", headlessVal);
+    test.equal(headlessVal, 'true');
     var filename = path.join(path.dirname(__filename), "nodejs.png");
     var file = java.newInstanceSync("java.io.File", filename);
     var image = java.callStaticMethodSync("javax.imageio.ImageIO", "read", file);
