d3775d06604dc777cd02893266550167ac6cccf1
Joe Ferner <joe@fernsroth.com>
2012-12-06 14:24:57 -0500

    enhance test

--- testIntegration/jdbc/enduranceTest.js
+++ testIntegration/jdbc/enduranceTest.js
@@ -19,8 +19,7 @@ java.classpath.push(path.join(__dirname, 'sqljdbc4.jar'));
 java.classpath.push(path.join(__dirname, 'mysql-connector-java-5.1.22-bin.jar'));
 var DriverManager = java.import('java.sql.DriverManager');
 
-var loopCount = 0;
-while (true) {
+for (var loopCount = 0; loopCount < 500000; loopCount++) {
   console.log('loopCount:', loopCount);
   java.findClassSync(dbConnectionClass);
   var conn = DriverManager.getConnectionSync(dbConnectString, dbUserId, dbPassword);
@@ -31,10 +30,14 @@ while (true) {
   var metaData = rs.getMetaDataSync();
   var columnCount = metaData.getColumnCountSync();
   while (rs.nextSync()) {
-    for(var i=1; i<=columnCount; i++) {
+    for (var i = 1; i <= columnCount; i++) {
       console.log(rs.getObjectSync(i));
     }
   }
   conn.closeSync();
-  loopCount++;
 }
+
+console.log("done... waiting 30seconds");
+setTimeout(function() {
+  console.log("really done");
+}, 30 * 1000);
