45b7351091a05ebbdb271863b5aa9200de01599d
Joe Ferner <joe@fernsroth.com>
2013-11-06 10:00:36 -0500

    added compile script

---
+++ compile-java-code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+JAVA_VERSION=1.6
+JAVAC_OPTS="-source ${JAVA_VERSION} -target ${JAVA_VERSION} -bootclasspath /opt/jdk1.6.0_45/jre/lib/rt.jar"
+
+cd test
+javac ${JAVAC_OPTS} *.java
+
+cd ../src-java/node
+javac ${JAVAC_OPTS} *.java
