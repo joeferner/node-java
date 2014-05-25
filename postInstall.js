a4c47f938903957f578c7f228f4aad34b6cfdb32
jsdevel <js.developer.undefined@gmail.com>
2014-05-25 01:16:14 -0700

    Attempting a fix for mavericks.

--- postInstall.js
+++ postInstall.js
@@ -4,13 +4,15 @@ var path = require('path');
 
 require('find-java-home')(function(err, home){
   var dll;
+  var dylib;
   var so;
   var binary;
 
   if(home){
     dll = glob.sync('**/jvm.dll', {cwd: home})[0];
     so = glob.sync('**/libjvm.so', {cwd: home})[0];
-    binary = dll || so;
+    dylib = glob.sync('**/libjvm.dylib', {cwd: home})[0];
+    binary = dll || dylib || so;
 
     fs.writeFileSync(
       path.resolve(__dirname, './build/jvm_dll_path.json'),
