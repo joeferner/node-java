8014db722394fb2bb17126f2e3aee5bf55b60bff
facboy <facboy@gmail.com>
2014-08-31 20:08:56 +0100

    Propagate node.js exceptions into Java.

---
+++ src-java/node/NodeJsException.java
@@ -0,0 +1,10 @@
+package node;
+
+/**
+ * @author Christopher Ng
+ */
+public class NodeJsException extends RuntimeException {
+    public NodeJsException(String message) {
+        super(message);
+    }
+}
