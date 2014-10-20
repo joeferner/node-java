a9584e03269921ceff4cb33b897ecf0de245c527
Jim Lloyd <jim.lloyd@gmail.com>
2014-10-20 13:13:59 -0700

    Simplify Java8 test & handle Unsupported major.minor vers exception.

--- test8/testLambda.js
+++ test8/testLambda.js
@@ -4,41 +4,24 @@ var nodeunit = require("nodeunit");
 var util = require("util");
 
 exports['Java8'] = nodeunit.testCase({
-
-  "create an instance of a class that uses lambda expressions": function(test) {
-    java.newInstance("TestLambda", function(err, lambda) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      test.ok(lambda);
-      if (lambda) {
-        lambda.getClass(function(err, result) {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          result.getName(function(err, result) {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            test.equal(result, "TestLambda");
-            test.done();
-          });
-        });
-      }
-    });
-  },
-
   "call methods of a class that uses lambda expressions": function(test) {
-    var TestLambda = java.import('TestLambda');
-    var lambda = new TestLambda();
-    var sum = lambda.testLambdaAdditionSync(23, 42);
-    test.equal(sum, 65);
-    var diff = lambda.testLambdaSubtractionSync(23, 42);
-    test.equal(diff, -19);
+    try {
+      var TestLambda = java.import('TestLambda');
+      var lambda = new TestLambda();
+      var sum = lambda.testLambdaAdditionSync(23, 42);
+      test.equal(sum, 65);
+      var diff = lambda.testLambdaSubtractionSync(23, 42);
+      test.equal(diff, -19);
+    }
+    catch (err) {
+      var unsupportedVersion = err.toString().match(/Unsupported major.minor version 52.0/)
+      test.ok(unsupportedVersion);
+      if (unsupportedVersion)
+        console.log('JRE 1.8 not available');
+      else
+        console.error('Java8 test failed with unknown error:', err);
+    }
     test.done();
-  },
+  }
 });
 
