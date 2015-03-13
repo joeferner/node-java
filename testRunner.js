399e7d07c008397ec59d58734778e0e3c9c03e7c
Jim Lloyd <jim.lloyd@gmail.com>
2015-03-13 11:52:07 -0700

    Implement testRunner.js.
    We need a convenient way to invoke nodeunit multiple
    times in independent processes. This testRunner runs
    the standard tests in the `test` and `test8` directory
    in on process, and then runs each of the files in
    testAsyncOptions, each in their own process.
    
    Perhaps I could have done this with a script, but
    it's unclear to me whether a shell style script
    would work on Windows. I expect this should be portable
    across all platforms.

---
+++ testRunner.js
@@ -0,0 +1,35 @@
+// testRunner.js
+
+// This is a custom test runner. All tests are run with nodeunit, but in separate
+// processes, which allows us to test java with different configuration options.
+
+var async = require('async');
+var chalk = require('chalk');
+var childProcess = require('child_process');
+var glob = require('glob');
+var path = require('path');
+
+var tests = glob.sync(path.join('testAsyncOptions', '*.js'));
+
+tests.unshift('test test8');  // Arrange to run the primary tests first, in a single process
+
+function runTest(testArgs, done) {
+  var cmd = 'node_modules/.bin/nodeunit ' + testArgs;
+  childProcess.exec(cmd, function (error, stdout, stderr) {
+    // It appears that nodeunit merges error output into the stdout
+    // so these three lines are probably useless.
+    var errText = stderr.toString();
+    if (errText !== '')
+      console.error(chalk.bold.red(errText));
+
+    process.stdout.write(stdout.toString());
+    done(error);
+  });
+}
+
+async.eachSeries(tests, runTest, function(err) {
+  if (err) {
+    console.error(chalk.bold.red(err));
+    process.exit(1);
+  }
+});
