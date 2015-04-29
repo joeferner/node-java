98c9b79a23f2c533b391095ecc01d83b89e06637
Jim Lloyd <jim.lloyd@gmail.com>
2015-04-29 09:44:31 -0700

    Use alternate name for static members with readonly property names

--- test/importClass-test.js
+++ test/importClass-test.js
@@ -3,6 +3,7 @@
 var java = require("../testHelpers").java;
 var nodeunit = require("nodeunit");
 var util = require("util");
+var _ = require("lodash");
 
 exports['Import Class'] = nodeunit.testCase({
   tearDown: function (callback) {
@@ -25,5 +26,43 @@ exports['Import Class'] = nodeunit.testCase({
       test.equals(5, testObj.getIntSync());
       test.done();
     });
+  },
+
+  "import TestEnum with unsable name": function (test) {
+    test.expect(5);
+    var TestEnum = java.import('Test$Enum');
+
+    // 'foo' and 'bar' are valid enum names
+    test.strictEqual(TestEnum.foo.toStringSync(), "foo");
+    test.strictEqual(TestEnum.bar.toStringSync(), "bar");
+
+    _.forEach(['name', 'arguments', 'caller'], function(prop) {
+      test.throws(
+        function() {
+          // The enum also defines 'name', 'caller', and 'attributes', but Javascript prevents us from using them,
+          // since these are unwritable properties of Function.
+          var x = TestEnum[prop].toStringSync();
+        },
+        TypeError
+      );
+    });
+    test.done();
+  },
+
+  "import TestEnum and use alternate name": function (test) {
+    test.expect(5);
+    var TestEnum = java.import('Test$Enum');
+
+    // 'foo' and 'bar' are valid enum names
+    test.strictEqual(TestEnum.foo.toStringSync(), "foo");
+    test.strictEqual(TestEnum.bar.toStringSync(), "bar");
+
+    // 'name', 'caller', and 'arguments' are not, so we must use e.g. 'name_' to reference the enum.
+    // But note that the value is still e.g. "name".
+    test.strictEqual(TestEnum.name_.toStringSync(), "name");
+    test.strictEqual(TestEnum.arguments_.toStringSync(), "arguments");
+    test.strictEqual(TestEnum.caller_.toStringSync(), "caller");
+    test.done();
   }
+
 });
