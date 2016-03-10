2a85f93e0b53b6d3b3ffb8a4c85743b899fa9df0
dyu <david.yu.ftw@gmail.com>
2016-03-10 08:41:52 +0800

    JAVA_HOME script arg

--- compile-java-code.sh
+++ compile-java-code.sh
@@ -15,6 +15,18 @@ if [ -x /usr/libexec/java_home ]; then
 
   cd ../../
   ${JAVA_HOME}/bin/javah -classpath src-java -d ./src node.NodeDynamicProxyClass
+elif [ -n "$1" ]; then
+  JAVA_HOME=$1
+  JAVAC_OPTS="-source ${JAVA_VERSION} -target ${JAVA_VERSION} -bootclasspath ${JAVA_HOME}/jre/lib/rt.jar"
+
+  cd test
+  ${JAVA_HOME}/bin/javac ${JAVAC_OPTS} *.java
+
+  cd ../src-java/node
+  ${JAVA_HOME}/bin/javac ${JAVAC_OPTS} *.java
+
+  cd ../../
+  ${JAVA_HOME}/bin/javah -classpath src-java -d ./src node.NodeDynamicProxyClass
 else
   JAVAC_OPTS="-source ${JAVA_VERSION} -target ${JAVA_VERSION} -bootclasspath /opt/jdk/jre/lib/rt.jar"
 
