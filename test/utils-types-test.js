7600d4982ce74f8bdf3fe8208e53e8d16615dd28
wsw <wsw0108@gmail.com>
2016-09-21 08:34:20 +0800

    support java char correctly

--- test/utils-types-test.js
+++ test/utils-types-test.js
@@ -50,13 +50,13 @@ exports['Utils - Types'] = nodeunit.testCase({
   },
 
   "Char array": function(test) {
-    var originalArray = "hello world\n".split('');
+    var originalArray = "hello 世界\n".split('');
     var Arrays = java.import("java.util.Arrays");
     var arr1 = java.newArray("char", originalArray);
     var list = Arrays.asListSync(arr1);
     var arr2 = list.toArraySync();
     test.equal(arr2.length, 1);
-    test.equal(arr2[0].length, 12);
+    test.equal(arr2[0].length, 9);
     for(var i=0; i<originalArray.length; i++) {
       test.equal(arr2[0][i], originalArray[i]);
     }
