88168d12446d91540a00ed78c18f144df2c9e234
Andy Fox <andyf0x@users.noreply.github.com>
2021-10-17 16:22:56 -0700

    Exit with error if Java home can't be found

--- findJavaHome.js
+++ findJavaHome.js
@@ -1,6 +1,6 @@
 require('find-java-home')(function(err, home){
-  if(err){
-    console.error("[node-java] "+err);
+  if (err || !home) {
+    if (!err) err = Error('Unable to determine Java home location');
     process.exit(1);
   }
   process.stdout.write(home);
